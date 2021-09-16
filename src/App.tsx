b7086dc6495981819ac527d998d9ebb3ef9d7462
Zlotkowski <zlotkowskimariusz@gmail.com>
2021-09-16 14:51:09 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,15 @@ import {
   initialProjects,
   clearProjects,
   addProject,
+  delProject,
+  getProjectNames,
+  addTask,
 } from "./store/projectsSlice";
 
 export default function App() {
   const [projectName, setProjectName] = useState("");
+  const [sectionName, setSectionName] = useState("");
+  const [taskName, setTaskName] = useState("");
   const [estimatedBy, setEstimatedBy] = useState("");
   const projects = useAppSelector((state) => state.projects.projects);
   const dispatch = useAppDispatch();
@@ -26,11 +31,15 @@ export default function App() {
 
   return (
     <div>
-      <p>---------- INITIAL PROJECT ----------</p>
+      <p>---------- INITIAL PROJECTS ----------</p>
       <button onClick={() => dispatch(initialProjects())}>
         initialProjects
       </button>
       <button onClick={() => dispatch(clearProjects())}>clearProjects</button>
+      <p>---------- GET PROJECTS NAMES ----------</p>
+      <button onClick={() => dispatch(getProjectNames())}>
+        getProjectNames
+      </button>
       <p>--------- ADD NEW PROJECT -----------</p>
       <input
         type="text"
@@ -54,6 +63,57 @@ export default function App() {
         }}
       >
         addProject
+      </button>
+      <p>--------- DEL PROJECT -----------</p>
+      <input
+        type="text"
+        value={projectName}
+        onChange={(event) => setProjectName(event.target.value)}
+        placeholder="Project Name To Del"
+      />
+      <button
+        onClick={() => {
+          dispatch(delProject({ projectName: projectName }));
+          setProjectName("");
+        }}
+      >
+        delProject
+      </button>
+      <p>--------- ADD TASK -----------</p>
+      Frontend development <br />
+      <br />
+      <input
+        type="text"
+        value={projectName}
+        onChange={(event) => setProjectName(event.target.value)}
+        placeholder="Project Name To Add Task"
+      />
+      <input
+        type="text"
+        value={sectionName}
+        onChange={(event) => setSectionName(event.target.value)}
+        placeholder="Section Name To Add Task"
+      />
+      <input
+        type="text"
+        value={taskName}
+        onChange={(event) => setTaskName(event.target.value)}
+        placeholder="Task Name To Add Task"
+      />
+      <button
+        onClick={() => {
+          dispatch(
+            addTask({
+              projectName: projectName,
+              sectionName: sectionName,
+              taskName: taskName,
+            })
+          );
+          setSectionName("");
+          setProjectName("");
+        }}
+      >
+        addTask
       </button>
     </div>
   );
--- src/store/projectsSlice.ts
+++ src/store/projectsSlice.ts
@@ -1,6 +1,6 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction, current } from "@reduxjs/toolkit";
 import data from "../data.json";
-import { Projects, Project } from "../types/Interface";
+import { Projects, Project, Task, Type, Role } from "../types/Interface";
 
 const initialState: Projects = {
   projects: [],
@@ -17,23 +17,106 @@ const initialProject: Project = {
   teamSize: 0,
   timeBudget: 0,
   effort: 0,
-  sections: [],
+  sections: [
+    {
+      sectionId: "",
+      name: "Frontend development",
+      minMd: 0,
+      minMdFormula: "",
+      maxMd: 0,
+      maxMdFormula: "",
+      predictedMd: 0,
+      predictedMdFormula: "",
+      risk: 0,
+      riskFormula: "",
+      tasks: [],
+    },
+    {
+      sectionId: "",
+      name: "Backend development",
+      minMd: 0,
+      minMdFormula: "",
+      maxMd: 0,
+      maxMdFormula: "",
+      predictedMd: 0,
+      predictedMdFormula: "",
+      risk: 0,
+      riskFormula: "",
+      tasks: [],
+    },
+    {
+      sectionId: "",
+      name: "Mobile development",
+      minMd: 0,
+      minMdFormula: "",
+      maxMd: 0,
+      maxMdFormula: "",
+      predictedMd: 0,
+      predictedMdFormula: "",
+      risk: 0,
+      riskFormula: "",
+      tasks: [],
+    },
+    {
+      sectionId: "",
+      name: "Design / UX / UI",
+      minMd: 0,
+      minMdFormula: "",
+      maxMd: 0,
+      maxMdFormula: "",
+      predictedMd: 0,
+      predictedMdFormula: "",
+      risk: 0,
+      riskFormula: "",
+      tasks: [],
+    },
+    {
+      sectionId: "",
+      name: "Configuration / Setup / Deployment",
+      minMd: 0,
+      minMdFormula: "",
+      maxMd: 0,
+      maxMdFormula: "",
+      predictedMd: 0,
+      predictedMdFormula: "",
+      risk: 0,
+      riskFormula: "",
+      tasks: [],
+    },
+  ],
   rawDevelopmentEffortSum: {
     name: "",
     main: {},
     parts: [],
   },
-
   summary: [],
   assumptions: [],
 };
 
+const initialTask: Task = {
+  id: 0,
+  type: Type.Group,
+  name: "",
+  role: Role.Empty,
+  minMd: 0,
+  maxMd: 0,
+  predictedMd: 0,
+  predictedMdFormula: "",
+  risk: "",
+  comment: {
+    text: "",
+    isImportant: false,
+  },
+
+  subtasks: [],
+};
+
 const shortDate = () => {
-  var dateObj = new Date();
-  var month = ("0" + (dateObj.getMonth() + 1)).slice(-2);
-  var date = ("0" + dateObj.getDate()).slice(-2);
-  var year = dateObj.getFullYear();
-  var shortDate = date + "." + month + "." + year;
+  const dateObj = new Date();
+  const month = ("0" + (dateObj.getMonth() + 1)).slice(-2);
+  const date = ("0" + dateObj.getDate()).slice(-2);
+  const year = dateObj.getFullYear();
+  const shortDate = date + "." + month + "." + year;
   return shortDate;
 };
 
@@ -53,12 +136,10 @@ const projectSlice = createSlice({
     clearProjects: (state) => {
       state.projects = [];
     },
+    getProjectNames: (state) => {
+      state.projects.map((project) => console.log(project.projectName));
+    },
     addProject: (state, action: PayloadAction<Project>) => {
-      //   var dateObj = new Date();
-      //   var month = ("0" + (dateObj.getMonth() + 1)).slice(-2);
-      //   var date = ("0" + dateObj.getDate()).slice(-2);
-      //   var year = dateObj.getFullYear();
-      //   var shortDate = date + "." + month + "." + year;
       const newProject = Object.assign({}, initialProject, {
         projectName: action.payload.projectName,
         estimatedBy: action.payload.estimatedBy,
@@ -66,17 +147,87 @@ const projectSlice = createSlice({
       });
       state.projects.push(newProject);
     },
+    delProject: (state, action: PayloadAction<any>) => {
+      state.projects.find(
+        (project) => project.projectName === action.payload.projectName
+      )
+        ? (state.projects = state.projects.filter(
+            (project) => project.projectName !== action.payload.projectName
+          ))
+        : console.log(
+            `Dont find project with name "${action.payload.projectName}"`
+          );
+    },
+    addTask: (state, action: PayloadAction<any>) => {
+      state.projects.find(
+        (project) => project.projectName === action.payload.projectName
+      )
+        ? console.log("Probably i make add")
+        : console.log(
+            `Dont find project with name "${action.payload.projectName}" ! soo.. i dont make add this that :(`
+          );
+    },
+    // addTask: (state, action: PayloadAction<any>) => {
+    //   //   const project = getProject(state.projects, action.payload.projectName);
+    //   //   const section = getSection(project, action.payload.sectionName);
+    //   //   //   state.projects.push(newProject);
+    //   //   const newProject = Object.assign({}, initialProject, {
+    //   //     projectName: action.payload.projectName,
+    //   //     estimatedBy: action.payload.estimatedBy,
+    //   //     estimationDate: shortDate(),
+    //   //   });
+    //   //   console.log(current(section));
+    //   ///------------
+    //   //   const newState = [...state.projects].map((project) =>
+    //   //     project.projectName === action.payload.projectName
+    //   //       ? { ...project, sections: [...project.sections] }
+    //   //       : project
+    //   //   );
+    // },
   },
 });
 
+// const getProject = (projects: Project[], chosenProject: any) => {
+//   const project = projects.find(
+//     (project) => project.projectName === chosenProject
+//   );
+//   return project
+//     ? project
+//     : console.log(
+//         `Dont find project with name "${chosenProject}" ! soo.. i dont make add this that :(`
+//       );
+// };
+
+// const getSection = (project: any, sectionName: string) => {
+//   if (!project) {
+//     return;
+//   }
+//   const section = project.sections.find(
+//     (sec: { name: string }) => sec.name === sectionName
+//   );
+//   return section
+//     ? section
+//     : console.log(
+//         `Dont find section with name "${section}" ! soo.. i dont make add this that :(`
+//       );
+// };
+
 // Action creators are generated for each case reducer function
-export const { initialProjects, clearProjects, addProject } =
-  projectSlice.actions;
+export const {
+  initialProjects,
+  clearProjects,
+  getProjectNames,
+  addProject,
+  delProject,
+  addTask,
+} = projectSlice.actions;
 
 const projectReducer = projectSlice.reducer;
 
 export default projectReducer;
 //add task
 //add sub
-//del task, sub, proj
+//del task
+//del sub
+//del proj -> done
 //add proj -> done
