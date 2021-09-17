b1c91bf3f06d1302224be5a347066ae401cf60ad
Piotr Dyrda <piotr.dyrda88@gmail.com>
2021-09-17 11:23:22 +0200

    modified app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import EstimateTable from './components/EstimateTable/EstimateTable';
 
 function App() {
-	return (
-		<div>
-			<EstimateTable />
-		</div>
-	);
+	return <div></div>;
 }
 
 export default App;
