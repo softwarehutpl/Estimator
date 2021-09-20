0bced624a2859e7e9af5bcc5614069f7b8133456
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2021-09-20 14:19:01 +0200

    Project List connected to the store

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,13 +1,21 @@
 import { Fieldset } from "primereact/fieldset";
 import ProjectTile from "../../components/Tiles/ProjectTile/ProjectTile";
 import CardTile from "../../components/Tiles/CardTile/CardTile";
+import { useAppSelector } from "../../store/hooks";
 import styles from "./home.module.scss";
 
 interface Props {}
 
-const data = ["First born project", "Second child", "Meh"];
-
 const Home = (props: Props) => {
+  const projectsData = useAppSelector((state) =>
+    state.projects.projects.map((project) =>
+      Object.create({
+        projectName: project.projectName,
+        projectId: project.projectId,
+      })
+    )
+  );
+  console.log(projectsData);
   return (
     <>
       <div className={`${styles.home} p-d-flex`}>
@@ -20,8 +28,8 @@ const Home = (props: Props) => {
       </div>
       <Fieldset legend="Projects" collapsed={false} toggleable>
         <ul className={styles.list}>
-          {data.map((project) => (
-            <ProjectTile key={project} title={project} />
+          {projectsData.map((project) => (
+            <ProjectTile key={project.projectId} title={project.projectName} />
           ))}
         </ul>
       </Fieldset>
