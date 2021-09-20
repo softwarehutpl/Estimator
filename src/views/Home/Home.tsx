dbef14568d6a535e5df288fa4c71b5d815cec7d0
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2021-09-20 10:32:29 +0200

    Added Project List

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,20 +1,33 @@
 import React from "react";
 import { Card } from "primereact/card";
 import { Button } from "primereact/button";
+import { Fieldset } from "primereact/fieldset";
+import ProjectTile from "../../components/ProjectTile/ProjectTile";
 import styles from "./home.module.scss";
 
 interface Props {}
 
+const data = ["First born project", "Second child", "Meh"];
+
 const Home = (props: Props) => {
   return (
-    <div className={`${styles.home} p-d-flex`}>
-      <Card title="Create Project">
-        <Button label="Create New" />
-      </Card>
-      <Card title="Select Project">
-        <Button label="Create" />
-      </Card>
-    </div>
+    <>
+      <div className={`${styles.home} p-d-flex`}>
+        <Card title="Create Project">
+          <Button label="Create New" />
+        </Card>
+        <Card title="Join">
+          <Button label="Join" />
+        </Card>
+      </div>
+      <Fieldset legend="Projects" toggleable>
+        <ul className={styles.list}>
+          {data.map((project) => (
+            <ProjectTile title={project} />
+          ))}
+        </ul>
+      </Fieldset>
+    </>
   );
 };
 
