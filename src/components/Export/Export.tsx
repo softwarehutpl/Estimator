820770ea1e52d380f83493441a56618970b0c764
alicjakarczewska <alicja.karczewska1@gmail.com>
2021-09-24 14:28:41 +0200

    done exporting function

--- src/components/Export/Export.tsx
+++ src/components/Export/Export.tsx
@@ -172,152 +172,10 @@ const totalValuesStyle: ReactExport.ExcelStyle = {
   }
 }
 
-
 const ExcelFile = ReactExport.ExcelFile;
 const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
 
-
 interface Props {}
-
-// const multiDataSet = [
-//   {
-//     columns: [
-//       { title: "Headings", width: { wpx: 80 } }, //pixels width
-//       { title: "Text Style", width: { wch: 40 } }, //char width
-//       { title: "Colors", width: { wpx: 90 } },
-//     ],
-//     data: [
-//       [
-//         { value: "H1", style: { font: { sz: "24", bold: true } } },
-//         { value: "Bold", style: { font: { bold: true } } },
-//         {
-//           value: "Red",
-//           style: {
-//             fill: { patternType: "solid", fgColor: { rgb: "FFFF0000" } },
-//           },
-//         },
-//       ],
-//       [
-//         { value: "H2", style: { font: { sz: "18", bold: true } } },
-//         { value: "underline", style: { font: { underline: true } } },
-//         {
-//           value: "Blue",
-//           style: {
-//             fill: { patternType: "solid", fgColor: { rgb: "FF0000FF" } },
-//           },
-//         },
-//       ],
-//       [
-//         { value: "H3", style: { font: { sz: "14", bold: true } } },
-//         { value: "italic", style: { font: { italic: true } } },
-//         {
-//           value: "Green",
-//           style: {
-//             fill: { patternType: "solid", fgColor: { rgb: "FF00FF00" } },
-//           },
-//         },
-//       ],
-//       [
-//         { value: "H4", style: { font: { sz: "12", bold: true } } },
-//         { value: "strike", style: { font: { strike: true } } },
-//         {
-//           value: "Orange",
-//           style: {
-//             fill: { patternType: "solid", fgColor: { rgb: "FFF86B00" } },
-//           },
-//         },
-//       ],
-//       [
-//         { value: "H5", style: { font: { sz: "10.5", bold: true } } },
-//         { value: "outline", style: { font: { outline: true } } },
-//         {
-//           value: "Yellow",
-//           style: {
-//             fill: { patternType: "solid", fgColor: { rgb: "FF000000" } },
-//           },
-//         },
-//       ],
-//       [
-//         { value: "H6", style: { font: { sz: "7.5", bold: true } } },
-//         { value: "shadow", style: { font: { shadow: true } } },
-//         {
-//           value: "Light Blue",
-//           style: {
-//             fill: { patternType: "solid", fgColor: { rgb: "FFCCEEFF" } },
-//           },
-//         },
-//       ],
-//     ],
-//   },
-// ];
-
-
-// const multiDataSet2 = [
-//   {
-//     columns: [
-//       { title: "A", width: { wpx: 30 } },
-//       { title: "B", width: { wpx: 81 } },
-//       { title: "C", width: { wpx: 142 } },
-//       { title: "D", width: { wpx: 35 } },
-//       { title: "E", width: { wpx: 85 } },
-//       { title: "F", width: { wpx: 81 } },
-//       { title: "G", width: { wpx: 81 } },
-//       { title: "H", width: { wpx: 81 } },
-//       { title: "I", width: { wpx: 51 } },
-//     ],
-//     data: [
-//       [        
-//         { value: initialProject.sections[3].name, style: { font: { sz: "24", bold: true} } },
-//         { value: "Bold", style: { font: { bold: true } } },
-//         {
-//           value: "Red",
-//           style: {
-//             fill: { patternType: "solid", fgColor: { rgb: "FFFF0000" } },
-//           },
-//         },
-//       ],
-//       [
-//         { value: "H1", style: { font: { sz: "24", bold: true} } },
-//         { value: "Bold", style: { font: { bold: true } } },
-//         {
-//           value: "Red",
-//           style: {
-//             fill: { patternType: "solid", fgColor: { rgb: "FFFF0000" } },
-//           },
-//         },
-
-//       ],
-//     ],
-//   },
-// ];
-
-// const myData = [ 
-//   {last_name: "somebody last name", name: "somebody name"},
-//   {last_name: "somebody last name", name: "somebody name"},
-// ]
-
-
-// const Export = (props: Props) => {
-
-//   // const project: Project = useAppSelector((state) =>
-// 	// 	state.projects.projects.find((project: Project) => project.projectId === projectId)
-// 	// );
-//   console.log(initialProject.sections[2])
-//   return (
-//     <>
-//     <div>
-//     <ExcelFile element={<button>Export</button>}>
-//         <ExcelSheet dataSet={multiDataSet} name="Organization" />
-//       </ExcelFile>
-
-//       <p>{multiDataSet[0].columns[0].title}</p>
-//     </div>
-    
-      
-//     </>
-//   );
-// };
-
 
 export default function Export() {
   
@@ -711,7 +569,6 @@ export default function Export() {
   };
 
   const dataSetToExport = createDataSetToExport(myProject);
-
 
   return (
     <div className="App">
