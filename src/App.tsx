4d7d0f5be83ab490fe3d8f2936540c860354e3b0
alicjakarczewska <alicja.karczewska1@gmail.com>
2021-09-27 10:35:45 +0200

    started import comonent

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import Nav from './components/Navigaton/Nav';
 import Home from './views/Home/Home';
@@ -8,16 +7,6 @@ import NotFound404 from './views/NotFound404/NotFound404';
 import { Route, Switch } from 'react-router-dom';
 import RootStore from './store/interface/RootStore';
 import Import from './components/Import/Import';
-=======
-import React from "react";
-import Nav from "./components/Navigaton/Nav";
-import Home from "./views/Home/Home";
-import Project from "./views/Project/Project";
-import Export from "./components/Export/Export";
-import NotFound404 from "./views/NotFound404/NotFound404";
-import { Route, Switch } from "react-router-dom";
-import RootStore from "./store/interface/RootStore";
->>>>>>> b7f312c7ebaf1d74f9906e5389f35a3990847b45
 
 function App() {
   return (
@@ -49,15 +38,11 @@ function App() {
           <Nav />
           <Export />
         </Route>
-<<<<<<< HEAD
         <Route path='/import'>
           <Nav />
           <Import />
         </Route>
         <Route path='*'>
-=======
-        <Route path="*">
->>>>>>> b7f312c7ebaf1d74f9906e5389f35a3990847b45
           <Nav />
           <NotFound404 />
         </Route>
