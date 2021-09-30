3c8372dc2293418938a41179783fd7315a5b5100
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2021-09-30 12:46:36 +0200

    Redirect 404

--- src/views/Project/Project.tsx
+++ src/views/Project/Project.tsx
@@ -1,31 +1,39 @@
-import { FC } from 'react';
+import { FC } from "react";
 //Components
-import EstimateTable from '../../components/Table/EstimateTable/EstimateTable';
-import ProjectSummary from '../../components/ProjectSummary/SummaryTable/SummaryTable';
-import DevelopmentEffortSummary from '../../components/DevelopmentEffortSummary/DevelopmentEffortSummary';
+import EstimateTable from "../../components/Table/EstimateTable/EstimateTable";
+import ProjectSummary from "../../components/ProjectSummary/SummaryTable/SummaryTable";
+import DevelopmentEffortSummary from "../../components/DevelopmentEffortSummary/DevelopmentEffortSummary";
 //Store
-import { useAppSelector } from '../../store/hooks';
-import { getProjectSelector } from '../../store/selectors/selectors';
+import { useAppSelector } from "../../store/hooks";
+import { getProjectSelector } from "../../store/selectors/selectors";
 //Router
-import { useParams } from 'react-router';
+import { useParams } from "react-router";
+import { Redirect } from "react-router-dom";
 //Types
 
-import { Project, Params } from '../../types/Interface';
+import { Project, Params } from "../../types/Interface";
 
 //Styles
-import styles from './project.module.scss';
+import styles from "./project.module.scss";
 
 interface Props {}
 
 const DataView: FC<Props> = () => {
   const { projectId } = useParams<Params>();
   const project: any = useAppSelector(getProjectSelector(projectId));
+  if (!project) {
+    return <Redirect to="/404" />;
+  }
   const devData = project.rawDevelopmentEffortSum;
   console.log(project);
   return (
     <main className={styles.contentWrapper}>
       <EstimateTable projectId={projectId} project={project} />
-      <DevelopmentEffortSummary projectId={projectId} summary={project.summary} data={devData} />
+      <DevelopmentEffortSummary
+        projectId={projectId}
+        summary={project.summary}
+        data={devData}
+      />
       <ProjectSummary projectId={projectId} summary={project.summary} />
     </main>
   );
